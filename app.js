6c81e8a00fca9d3aaffdf05e44a1c3013c537a08
Charlene149611 <c.gausset911@gmail.com>
2025-06-27 20:06:33 +0200

    optimisation du code

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
+console.log("app.js fonctionne !");
+
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import sqlite3 from "sqlite3";
 import bodyParser from "body-parser";
-import twig from "twig";
 
 const PORT = process.env.PORT || 3010;
 const app = express();
 
-app.set("view engine", "twig");
-app.set("views", "./views");
-
+app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "twig");
+app.set("views", "./views");
 
 //Création de la base de données
 
 const db = new sqlite3.Database("./todo.db");
+
+//Création de la table
 
 db.run(`
     CREATE TABLE IF NOT EXISTS todos(
@@ -27,7 +31,7 @@ db.run(`
     created_at TEXT DEFAULT CURRENT_TIMESTAMP)
     `);
 
-//CREATE
+//CREATE (tâche)
 app.post("/todo/create", (req, res) => {
   const { title, firstname } = req.body;
   db.run(
@@ -40,34 +44,35 @@ app.post("/todo/create", (req, res) => {
   );
 });
 
-//READ
+//READ (afficher les tâches)
 app.get("/", (req, res) => {
+  console.log("Route / appelée !");
   db.all("SELECT * FROM todos ORDER BY created_at DESC", (err, rows) => {
     if (err) return res.status(500).json({ error: err.message });
-    res.render("index", { todos: rows });
+    res.render("index", { rows }); // une liste de tâches envoyée depuis Express
   });
 });
 
-//DELETE
-app.delete("/todo/:id", (req, res) => {
+//DELETE (supprimer une tâche)
+app.post("/todo/:id/delete", (req, res) => {
   const { id } = req.params;
   db.run(`DELETE FROM todos WHERE id = ?`, id, function (err) {
     if (err) return res.status(500).json({ error: err.message });
-    res.json({ delete: this.changes });
+    res.redirect("/");
   });
 });
 
-//UPDATE
+//UPDATE (mise à jour du du status d'une tâche)
 app.put("/todo/:id", (req, res) => {
-  const { title, firstname, done } = req.body;
+  const { done } = req.body;
   const { id } = req.params;
 
   db.run(
-    "UPDATE todos SET title = ?, firstname = ?, done = ? WHERE id =?",
-    [title, firstname, done ? 1 : 0, id],
+    "UPDATE todos SET done = ? WHERE id =?",
+    [done ? 1 : 0, id],
     function (err) {
       if (err) return res.status(500).json({ error: err.message });
-      res.json({ update: this.changes });
+      res.status(201).json({ update: this.changes });
     }
   );
 });
