7a015a91209c1ecb225f94414aca2f8539f098e0
navycrow <hg64off@gmail.com>
2025-06-27 16:39:17 +0200

    ajout suppression

---
+++ public/script.js
@@ -0,0 +1,13 @@
+// Change le statut de la tâche
+const modifierTache = (e) => {
+    alert("modifier tache");
+};
+
+// Supprime la tâche et rafraichit la page
+const supprimerTache = (e) => {
+    const id = e.value;
+    console.log(id);
+    fetch(`/todos/delete/${id}`, { method: "DELETE" })
+        .then(() => location.reload())
+        .catch((err) => console.error(err));
+};
